bc2f7e31e049acb3fffa0fe06ee8d3c7ed485f9f
Ben <jaminscript@gmail.com>
2014-09-16 11:23:33 +1200

    Update the form with more elements. Set rules for the new form elements

--- application/controllers/registration.php
+++ application/controllers/registration.php
@@ -16,7 +16,9 @@ class Registration extends CI_Controller {
 		// arg1 = name of form element
 		// arg2 = Part of Error Message
 		// arg3 = list of validation steps separated by pipes. First to last
-		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[users.Username]');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[40]|is_unique[users.Username]|alpha_dash');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|max_length[60]');
+		$this->form_validation->set_rules('password2', 'Confirm Password', 'required|min_length[8]|max_length[60]|matches[password]');
 
 		// Get page data
 		$pageData = $this->page->getPageData('registration');
--- application/views/registration.php
+++ application/views/registration.php
@@ -3,19 +3,31 @@
 <p>Use this form to register an account</p>
 
 <?php
-
+	// Form settings
+	$usernameSettings 			= ['name'=>'username', 'id'=>'username'];
+	$passwordSettings 			= ['name'=>'password', 'id'=>'password'];
+	$confirmPasswordSettings 	= ['name'=>'password2', 'id'=>'password2'];
+	// The form
 	echo form_open('registration');
-
-	// Prepare settings for username input
-	$usernameSettings = ['name'=>'username', 'id'=>'username'];
-
-	echo form_label('Username: ', 'username');
-	echo form_input($usernameSettings);
-
-	echo form_submit('register', 'Register Account');
-
+?>
+<div>
+	<?php echo form_label('Username: ', 'username'); ?>
+	<?php echo form_input($usernameSettings); ?>
+</div>
+<div>
+	<?php echo form_label('Password: ', 'password'); ?>
+	<?php echo form_password($passwordSettings); ?>
+</div>
+<div>
+	<?php echo form_label('Confirm Password: ', 'password2'); ?>
+	<?php echo form_password($confirmPasswordSettings); ?>
+</div>
+<div>
+	<?php echo form_submit('register', 'Register Account'); ?>
+</div>
+<?php
+	// Close the form
 	echo form_close();
-
+	// All error messages are placed here
 	echo validation_errors();
-
 ?>
