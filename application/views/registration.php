5514c0c6ded5f4ceee09cbc5a63d1bae23ed4155
Ben <jaminscript@gmail.com>
2014-09-16 09:14:06 +1200

    Initial save.

---
+++ application/views/registration.php
@@ -0,0 +1,21 @@
+<h1>Register an account</h1>
+
+<p>Use this form to register an account</p>
+
+<?php
+
+	echo form_open('registration/doRegistration');
+
+	// Prepare settings for username input
+	$usernameSettings = ['name'=>'username', 'id'=>'username'];
+
+	echo form_label('Username: ', 'username');
+	echo form_input($usernameSettings);
+
+	echo form_submit('register', 'Register Account');
+
+	echo form_close();
+
+	echo validation_errors();
+
+?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $Page_Title; ?></title>
+	<meta name="description" content="<?php echo $Page_Meta; ?>">
+</head>
+<body>
+
+<nav>
+	<ul>
+		<li><?php echo anchor('home', 'Home'); ?></li>
+		<li><?php echo anchor('about', 'About'); ?></li>
+		<li><?php echo anchor('registration', 'Registration'); ?></li>
+	</ul>
+</nav>
