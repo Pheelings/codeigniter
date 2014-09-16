b8c9358bc4bdd6fb583235da921d786753cd6972
Stefan Roth-Thomas <stefanroththomas@gmail.com>
2014-09-17 09:07:11 +1200

    9/11 never gorget

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<p>&copy; <?php echo date('Y'); ?> Your Company Name</p>
+	<p>&copy; <?php echo date('Y'); ?> <strong>9/11 Never Forget</strong></p>
 </footer>
 
 <!-- JavaScript goes here -->
